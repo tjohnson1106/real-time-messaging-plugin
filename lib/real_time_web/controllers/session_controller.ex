037ac6480bcfbb300264b5ecf91b0439f766a373
Thomas Johnson <tjohnson1106@gmail.com>
2019-09-09 19:14:20 -0400

    created sign in form and create method in session controller

--- lib/real_time_web/controllers/session_controller.ex
+++ lib/real_time_web/controllers/session_controller.ex
@@ -4,4 +4,22 @@ defmodule RealTimeWeb.SessionController do
   def new(conn, _) do
     render(conn, "new.html")
   end
+
+  def create(
+        conn,
+        %{"email" => email, "password" => password}
+      ) do
+    case Accounts.sign_in(email, password) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user_id, user.id)
+        |> put_flash(:info, "You've signed in")
+        |> redirect(to: Routes.room_path(conn, :index))
+
+      {:error, _} ->
+        conn
+        |> put_flash(:error, "Invalid Email or Password")
+        |> render("new.html")
+    end
+  end
 end
