8ec60f75288f3173f43643582d184fdc4e0abd40
Thomas Johnson <tjohnson1106@gmail.com>
2019-10-21 19:24:49 -0400

    created token verification connect method

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -10,41 +10,45 @@ import { Socket } from "phoenix";
 
 let socket = new Socket("/socket", { params: { token: window.userToken } });
 
+let roomId = window.roomId;
+
 socket.connect();
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("room:general", {});
-channel
-  .join()
-  .receive("ok", (resp) => {
-    console.log("Joined successfully", resp);
-  })
-  .receive("error", (resp) => {
-    console.log("Unable to join", resp);
+if (roomId) {
+  // Now that you are connected, you can join channels with a topic:
+  let channel = socket.channel(`room:${roomId}`, {});
+  channel
+    .join()
+    .receive("ok", (resp) => {
+      console.log("Joined successfully", resp);
+    })
+    .receive("error", (resp) => {
+      console.log("Unable to join", resp);
+    });
+
+  channel.on(`room:${roomId}:new_message`, (message) => {
+    console.log(message);
+    displayMessage(message);
   });
 
-channel.on("room:general:new_message", (message) => {
-  console.log(message);
-  displayMessage(message);
-});
+  document.querySelector("#message-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let input = e.target.querySelector("#message-body");
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  let input = e.target.querySelector("#message-body");
+    channel.push("message:add", { message: input.value });
 
-  channel.push("message:add", { message: input.value });
+    input.value = "";
+  });
 
-  input.value = "";
-});
-
-const displayMessage = (msg) => {
-  let template = `
+  const displayMessage = (msg) => {
+    let template = `
   <li class="list-group-item">
   ${msg.body}
   </li>
   `;
 
-  document.querySelector("#display").innerHTML += template;
-};
+    document.querySelector("#display").innerHTML += template;
+  };
+}
 
 export default socket;
--- lib/real_time_web/channels/room_channel.ex
+++ lib/real_time_web/channels/room_channel.ex
@@ -1,12 +1,13 @@
 defmodule RealTimeWeb.RoomChannel do
   use RealTimeWeb, :channel
 
-  def join(channel_name, _params, socket) do
-    {:ok, %{channel: channel_name}, socket}
+  def join("room" <> room_id, _params, socket) do
+    {:ok, %{channel: "room:#{room_id}"}, assign(socket, :room_id, room_id)}
   end
 
   def handle_in("message:add", %{"message" => body}, socket) do
-    broadcast!(socket, "room:general:new_message", %{body: body})
+    room_id = socket.assigns[:room_id]
+    broadcast!(socket, "room:#{room_id}:new_message", %{body: body})
     {:reply, :ok, socket}
   end
 end
